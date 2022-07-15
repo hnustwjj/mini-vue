313b37e39a16b37cfd2103746beb6faf0edbb6a3
wjj <948065841@qq.com>
2022-07-15 15:44:28 +0800

    feat: 在调用effect的时候返回runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -6,13 +6,18 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this
-    this._fn()
+    const res = this._fn()
     activeEffect = null
+    // 返回fn的返回值
+    return res
   }
 }
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
   _effect.run()
+  // 因为run方法内部是用this指针来修改全局的activeEffect
+  // 所以这里需要修改this指向
+  return _effect.run.bind(_effect)
 }
 
 const targetMap = new WeakMap()
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -15,4 +15,15 @@ describe('effect', () => {
     user.age = 20
     expect(newAge).toBe(21)
   })
+
+  it('return runner when call effect', () => {
+    let foo = 10
+    // 调用effect会返回一个runner
+    const runner = effect(() => ++foo)
+    expect(foo).toBe(11)
+    // 调用runner会返回传入的函数的执行结果
+    const res = runner()
+    expect(res).toBe(12)
+    expect(foo).toBe(12)
+  })
 })
